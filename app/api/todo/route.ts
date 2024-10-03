2d55f96b022c58a3dc7d6634d576a97e9110d70d
yeongwoo <jyy0223@naver.com>
2024-10-04 02:17:47 +0900

    [feat] Router Handler에서 Server Action을 사용해 controller-service layer로 구분되도록 수정

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,13 +1,8 @@
 import {NextResponse} from "next/server";
-import {createSupabaseServerClient} from "@/lib/supabase";
+import {getTodoAction} from "@/actions/todo/todo.action";
 
 export const GET = async () => {
-
-  const supabase = await createSupabaseServerClient();
-  const result = await supabase.from("todos_no_rls")
-    .select("*")
-    .is("deleted_at", null)
-    .order("id", {ascending: false});
+  const result = await getTodoAction();
 
   console.log("todo GET API income", result);
   return NextResponse.json({...result});
