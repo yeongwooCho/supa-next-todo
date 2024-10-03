9d546ada9370b4e78a2efe578fd877563e266595
yeongwoo <jyy0223@naver.com>
2024-10-04 02:20:45 +0900

    [feat] AppRouter의 Server Side Rendering에서 ServerAction을 사용

--- app/todo/page.tsx
+++ app/todo/page.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import {pingAction} from "@/actions/ping/ping.action";
 import ClientComponentTest from "@/app/todo/components/client-component-test";
+import {getTodoAction} from "@/actions/todo/todo.action";
 
 const page = async () => {
 
   console.log(">>> SSR Start");
 
-  const result = await pingAction();
+  const result = await getTodoAction();
 
   console.log(">>> SSR End");
 
   return (
     <div>
-      <h1>server actions result: {result}</h1>
-      <ClientComponentTest />
+      <h1>server actions result: {JSON.stringify(result)}</h1>
+      <ClientComponentTest/>
     </div>
   );
 };
