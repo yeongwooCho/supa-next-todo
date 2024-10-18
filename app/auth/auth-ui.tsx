b6ca92e85918ee2e6a1848666fe3480638a7d1e1
yeongwoo <jyy0223@naver.com>
2024-10-18 15:27:27 +0900

    [feat] 사용자 정보 가져오기 및 로그아웃 기능 구현

--- app/auth/auth-ui.tsx
+++ app/auth/auth-ui.tsx
@@ -1,19 +1,41 @@
 "use client";
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Auth} from "@supabase/auth-ui-react";
 import {createSupabaseBrowserClient} from "@/lib/client/supabase";
 import {ThemeSupa} from "@supabase/auth-ui-shared";
 import {useHydrate} from "@/hooks/use-hydrate";
 
 const AuthUi = () => {
+  const [user, setUser] = useState('');
+
   const supabase = createSupabaseBrowserClient();
   const isMount = useHydrate();
+
+  const getUserInfo = async () => {
+    const result = await supabase.auth.getSession();
+    if (result.data.session?.user.email) {
+      setUser(result.data.session.user.email);
+    }
+  }
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    setUser('');
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
 
   if (!isMount) return null;
 
   return (
     <section className="w-full">
+      <div>{user ? `로그인 됨 ${user}` : "로그아웃 됨"}</div>
+      <div>
+        <button className="border" onClick={handleLogout}>로그아웃</button>
+      </div>
       <div className="mx-auto max-w-[500px]">
         <Auth
           redirectTo={process.env.NEXT_PUBLIC_AUTH_REDIRECT_TO}
