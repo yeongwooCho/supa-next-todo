5b5491ff4cf440ddd7c3eb79eb724c2f19fbf56f
yeongwoo <jyy0223@naver.com>
2024-10-18 20:07:02 +0900

    [feat] supabase에서 auth schema에 접근이 불가능해 profile table 생성 후 이를 통해 user info를 가져오도록 구현: 유저정보에 따른 공유페이지 redirect 로직 구현

--- actions/auth/user.action.ts
+++ actions/auth/user.action.ts
@@ -10,3 +10,11 @@ export const getUser = async ({serverComponent = false}) => {
 
   return user?.data?.user;
 };
+
+export const getProfileById = async ({serverComponent = false, userId = ""}) => {
+  const supabase = await createSupabaseServerClient(serverComponent);
+
+  const profile = await supabase.from("profiles").select("*").eq("id", userId);
+
+  return profile?.data?.[0];
+}
--- app/share/[user_id]/page.tsx
+++ app/share/[user_id]/page.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {getProfileById} from "@/actions/auth/user.action";
+import {permanentRedirect} from "next/navigation";
 
 interface ISharePageProps {
   params: { user_id: string };
 }
 
 // http://localhost:3000/share/cde553b9-27bf-44be-9be0-6632645cab37
-const page: React.FC<ISharePageProps> = ({params}) => {
+const page: React.FC<ISharePageProps> = async ({params}) => {
+  const userId = params.user_id;
+  const profile = await getProfileById({serverComponent: true, userId});
+  console.log('>>profile', profile);
+
+  if (!profile) {
+    permanentRedirect("/");
+  }
+
   return (
     <div>
       share page
