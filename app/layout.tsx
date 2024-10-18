cca2e05cd4758531b997cc65682f775540521743
yeongwoo <jyy0223@naver.com>
2024-10-18 19:21:35 +0900

    [feat] root layout에서 user 정보를 가져와 auth header에 전달

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default async function RootLayout({
                                          }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = await getUser();
+  const user = await getUser({serverComponent: true});
 
   return (
     <html lang="en">
